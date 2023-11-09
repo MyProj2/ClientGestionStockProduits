ffb75e06da0435f57869760f71a0e27899b8bad8
MyProj2 <mouakharyasmine@gmail.com>
2023-11-09 10:45:17 +0100

    Ajouter les bibliothèques Bootstrap, Font Awesome, jQuery, et Popper.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ProduitMockService } from './produit/produit.mock.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  providers: [ProduitMockService], 
+  bootstrap:[AppComponent]
 })
 export class AppModule { }
